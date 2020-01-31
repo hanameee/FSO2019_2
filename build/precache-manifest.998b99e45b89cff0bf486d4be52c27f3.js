02a5270c306101f0cbad172ad3f64042536f2952
hanameee <hanameee@gmail.com>
2020-01-31 17:44:38 +0900

    Add frontend build folder

---
+++ build/precache-manifest.998b99e45b89cff0bf486d4be52c27f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7592dff49a50ad929dcf7983d6636fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064159f62dbea6751637",
+    "url": "/static/css/main.2f32588f.chunk.css"
+  },
+  {
+    "revision": "380ef76a645476d32096",
+    "url": "/static/js/2.312ddd00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.312ddd00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "064159f62dbea6751637",
+    "url": "/static/js/main.25e7c56c.chunk.js"
+  },
+  {
+    "revision": "7cbb070ae2e349df2854",
+    "url": "/static/js/runtime-main.a161d986.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+app.use(express.static("build"));
 app.use(bodyParser.json());
 app.use(cors());
 morgan.token("data", function(req, res) {
