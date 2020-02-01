33a2c7777fd395ad81b84f82e93b8f4faed7cc62
hanameee <hanameee@gmail.com>
2020-02-01 20:26:36 +0900

    Change index.js to notes application

--- index.js
+++ index.js
@@ -14,86 +14,85 @@ app.use(
         ":method :url :status :res[content-length] - :response-time ms :data"
     )
 );
-let persons = [
+let notes = [
     {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
+        id: 1,
+        content: "HTML is easy",
+        date: "2019-05-30T17:30:31.098Z",
+        important: true
     },
     {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30T18:39:34.091Z",
+        important: false
     },
     {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
     }
 ];
 
+// define routes
 app.get("/", (req, res) => {
-    res.send("<h1>Ch3 Phonebook Exercise</h1>");
+    res.send("<h1>hello world!</h1>");
 });
 
-app.get("/info", (req, res) => {
-    const numOfPersons = persons.length;
-    const currentTime = new Date();
-    res.send(`<div>Phonebook has info for <b>${numOfPersons}</b> persons</div>
-    <div>${currentTime}</div>`);
+// get all notes
+app.get("/notes", (req, res) => {
+    res.json(notes);
 });
 
-app.get("/api/persons", (req, res) => {
-    res.json(persons);
-});
-
-app.get("/api/persons/:id", (req, res) => {
+// fetch specific note
+app.get("/notes/:id", (req, res) => {
+    // request의 params로 넘어오는 id는 type이 string이기에 number로 변환해줘야 find method를 사용할 수 있다
     const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        res.json(person);
+    const note = notes.find(
+        note =>
+            // console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+            note.id === id
+    );
+    if (note) {
+        res.json(note);
     } else {
         res.status(404).end();
     }
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+// delete specific note
+app.delete("/notes/:id", (req, res) => {
     const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
+    notes = notes.filter(note => note.id !== id);
+    // status code 204 : no content
     res.status(204).end();
 });
 
+// logic for generating new id
 const generateId = () => {
-    return Math.floor(Math.random() * 1000000);
+    const maxId =
+        notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0;
+    console.log(maxId, "maxId");
+    return maxId + 1;
 };
-
-app.post("/api/persons", (req, res) => {
+// post note and return that note - bodyparser required
+app.post("/notes", (req, res) => {
     const body = req.body;
-    if (!body.name) {
+    if (!body.content) {
+        // status code 400 : bad request
         return res.status(400).json({
-            error: "name missing"
-        });
-    } else if (!body.number) {
-        return res.status(400).json({
-            error: "number missing"
-        });
-    } else if (persons.find(person => person.name === body.name)) {
-        return res.status(400).json({
-            error: "name must be unique"
+            error: "content missing"
         });
     }
-    const person = {
-        name: body.name,
-        number: body.number,
+    const note = {
+        content: body.content,
+        important: body.imporatant || false,
+        date: new Date(),
         id: generateId()
     };
-    persons = persons.concat(person);
-    res.json(person);
+    notes = notes.concat(note);
+    res.json(note);
 });
 
 const unknownEndpoint = (request, response) => {
---
+++ lectures/3.b/Lb_index.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+app.use(express.static("build"));
+app.use(bodyParser.json());
+app.use(cors());
+morgan.token("data", function(req, res) {
+    return JSON.stringify(req.body);
+});
+app.use(
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :data"
+    )
+);
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    }
+];
+
+app.get("/", (req, res) => {
+    res.send("<h1>Ch3 Phonebook Exercise</h1>");
+});
+
+app.get("/info", (req, res) => {
+    const numOfPersons = persons.length;
+    const currentTime = new Date();
+    res.send(`<div>Phonebook has info for <b>${numOfPersons}</b> persons</div>
+    <div>${currentTime}</div>`);
+});
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+    res.status(204).end();
+});
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000);
+};
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body;
+    if (!body.name) {
+        return res.status(400).json({
+            error: "name missing"
+        });
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: "number missing"
+        });
+    } else if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: "name must be unique"
+        });
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    };
+    persons = persons.concat(person);
+    res.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
