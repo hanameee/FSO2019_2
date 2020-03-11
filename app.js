50a60fdd4c2073b536e5b47e672466e8650470c9
hanameee <hanameee@gmail.com>
2020-03-11 19:11:35 +0900

    Install bcrypt and made router for handling user

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
+
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
@@ -28,6 +31,7 @@ app.use(bodyParser.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
